e7f52ebed2abf9189d9d737677a04903d7cc2061
Jungmin <wakalics@gmail.com>
2020-08-30 19:10:59 +0900

    Done until lecture 6

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/*
+const mysql = require('mysql2');
+const connection = mysql.createConnection({
+  host: 'database-1.c2qix7nmsmbv.ap-northeast-2.rds.amazonaws.com',
+  user: '******',
+  password: '******',
+  database: 'School'
+})
+
+// GET users listing. 
+router.get('/', (req, res, next) => {
+  connection.query('SELECT * FROM USER_TB', (err, results) => {
+    const firstUser = results[0];
+    const userId = firstUser.id;
+    connection.query('SELECT * FROM REPORT_TB WHERE user_id = ?', [userId], (err2, results2) => {
+      res.json({ status: 200, arr: results2 });
+    })
+  })
 });
 
 router.get('/hoho', function(req, res, next) {
@@ -13,5 +28,39 @@ router.get('/hoho', function(req, res, next) {
 router.get('/hohoho', function(req, res, next) {
   res.json({name: "jungmin", age: "24", gender:"man"});
 });
+*/
+
+const pool = require('../utils/mysql');
+
+/*
+// GET users listing. 
+router.get('/', async (req, res, next) => {
+  try {
+    const connection = await pool.getConnection();
+    const [results] = await connection.query('SELECT * FROM USER_TB');
+    connection.release();
+    res.json({ status: 200, arr: results });
+  } catch (err) {
+    console.log(err);
+    res.json({ status: 500 , msg: 'Server Error!'});
+  }
+ 
+});
+*/
+
+router.post('/', async (req, res, next) => {
+  try {
+    const email = req.body.email;
+    const connection = await pool.getConnection();
+    await connection.query('INSERT INTO USER_TB(email) VALUES(?)', [email]);
+    connection.release();
+    res.json({ status: 201, msg: 'Success!'});
+  } catch (err) {
+    console.log(err);
+    res.json({ status: 500 , msg: 'Server Error!'});
+  }
+ 
+});
+
 
 module.exports = router;
