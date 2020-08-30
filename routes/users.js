693f941784f5a0e35073fb4362b2a04d57cbe502
Jungmin <wakalics@gmail.com>
2020-08-30 14:53:18 +0900

    Joker

--- routes/users.js
+++ routes/users.js
@@ -14,4 +14,12 @@ router.get('/hohoho', function(req, res, next) {
   res.json({name: "jungmin", age: "24", gender:"man"});
 });
 
+router.get('/hohohoho', function(req, res, next) {
+  res.send('hoho hoho hoho hoho');
+});
+
+router.get('/hohohohoho', function(req, res, next) {
+  res.send('hoho hoho hoho hoho hoho');
+});
+
 module.exports = router;
